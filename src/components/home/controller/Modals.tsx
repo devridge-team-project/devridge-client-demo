c7f9c188e17312568879a0e4125aed3144ade5bd
Park Minhyeong <duncansITworld@gmail.com>
2023-12-27 18:36:22 +0900

    [Style] solve the problem in the conflict between libraries and config files

--- src/components/home/controller/Modals.tsx
+++ src/components/home/controller/Modals.tsx
@@ -1,6 +1,7 @@
-import { useWidgetStore } from "@/shared/store";
+import { useWidgetStore } from "../../../shared/store";
+
 export default function Modals() {
   const { nowModal } = useWidgetStore();
   if (nowModal === "someModal") return <div>Some Modal</div>;
-  return <></>;
+  return <div />;
 }
--- src/components/home/view/Test.tsx
+++ src/components/home/view/Test.tsx
@@ -1,8 +1,14 @@
-import Template from "@/design/Template";
+import Template from "../../../design/Template";
+
 export default function Test() {
   return (
     <div>
-      <div className="text-2xl font-bold">ㅎㅇ</div>
+      <div
+        className="w-64 bg-black
+			 text-2xl font-bold"
+      >
+        ㅎㅇ
+      </div>
       <Template subject="This is Template's Subject">
         <div>This is Template's Children </div>
       </Template>
