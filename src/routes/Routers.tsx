672033620809e1f5ff55f18e6795bacce4c2b014
Park Minhyeong <duncansITworld@gmail.com>
2024-01-01 14:08:23 +0900

    [Fix] fix path

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../components/home/Home";
-import Login from "../components/sign-in/SignIn";
+import SignIn from "../components/sign-in/SignIn";
 import SignUp from "../components/sign-up/SignUp";
 
 export default function Routers() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   );
 }
