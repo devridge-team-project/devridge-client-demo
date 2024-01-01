5d80c84070843d387757c6ab24a4f0c6aa753b86
Park Minhyeong <duncansITworld@gmail.com>
2024-01-01 16:04:52 +0900

    [Refactor] seperate module

---
+++ src/components/sign-up/Page.tsx
@@ -0,0 +1,5 @@
+import SignUp from "./view/SignUp";
+
+export default function SignUpPage() {
+  return <SignUp />;
+}
--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../components/home/Home";
 import SignIn from "../components/sign-in/SignIn";
-import SignUp from "../components/sign-up/SignUp";
+import SignUpPage from "../components/sign-up/Page";
 
 export default function Routers() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-up" element={<SignUpPage />} />
     </Routes>
   );
 }
