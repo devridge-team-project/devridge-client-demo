8d193f1f532b5d3efd158ab3638bf333bf67687e
Park Minhyeong <duncansITworld@gmail.com>
2024-01-16 22:47:30 +0900

    [Feat] add feature find skill

--- src/components/sign-up/view/join/Skills.tsx
+++ src/components/sign-up/view/join/Skills.tsx
@@ -6,12 +6,16 @@ import { useWidgetStore } from "shared/store";
 import { col } from "style/display";
 
 export default function Skills() {
-  const [searchSkill, setSearchSkill] = useState("");
+  const [keyword, setKeyword] = useState("");
   const { setView } = useWidgetStore();
   const { skills, setSkills } = useSignUpStore();
 
   const onChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchSkill(e.target.value);
+    setKeyword(e.target.value);
+  };
+  const findSkill = (tech: string[]) => {
+    const result = techs.filter((tech) => tech.includes(keyword));
+    return result;
   };
 
   return (
@@ -21,14 +25,14 @@ export default function Skills() {
     >
       <input
         className="w-full rounded-md border-2 p-4 focus:outline-blue-500"
-        value={searchSkill}
+        value={keyword}
         onChange={onChangeValue}
         placeholder="스킬을 검색해보세요."
       />
       <div className={col(2)}>
         <div className="font-bold">빠른 선택</div>
-        <div className="flex h-84 w-full flex-wrap gap-2 overflow-hidden">
-          {techs.map((tech) => (
+        <div className="flex max-h-84 w-full flex-wrap gap-2 overflow-hidden">
+          {findSkill(techs).map((tech) => (
             <button
               key={tech}
               onClick={() => setSkills(tech)}
