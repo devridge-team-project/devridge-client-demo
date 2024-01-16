27b2a6e006622c9e7b9f3ea893f65c43572d1ef6
jyj1111 <jyj202040@gmail.com>
2024-01-16 18:43:31 +0900

    [Feat]: add logout api

--- src/api/sign-in/loginService.ts
+++ src/api/sign-in/loginService.ts
@@ -1,23 +1,37 @@
 import axios, { AxiosError } from "axios";
+
 import { LoginResponse, LoginRequest } from "../../interface/Login";
 import { axiosJsonInstance } from "../axios";
 
 export const login = async (user: LoginRequest) => {
   try {
-    const response: LoginResponse = await axiosJsonInstance.post("/login", user);
+    const { data, status } = await axiosJsonInstance.post("/login", user);
     const {
-      message,
-      body: { accessToken, status },
-    } = response;
-    console.log(response);
+      body: { accessToken },
+    } = data;
+    console.log(data);
     console.log(accessToken);
-    axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+    // axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+    return { status, accessToken };
+  } catch (error) {
+    if (error instanceof AxiosError && error.response?.status) {
+      const { status } = error.response;
+      return { status, accessToken: null };
+    }
+    return { status: 500, accessToken: null };
+  }
+};
+
+export const logout = async () => {
+  try {
+    const { status } = await axiosJsonInstance.get("/logout");
     return status;
   } catch (error) {
     if (error instanceof AxiosError && error.response?.status) {
-      console.log(error.response?.status);
-      return error.response?.status;
+      const { status } = error.response;
+      return status;
     }
-    return null;
+    return 500;
   }
 };
