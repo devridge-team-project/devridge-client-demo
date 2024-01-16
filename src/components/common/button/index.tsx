b5614652dfdf83c9370b2da1c0b719459b3bf1d8
jyj1111 <jyj202040@gmail.com>
2024-01-16 18:40:43 +0900

    [Feat]: add onClick type

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -4,11 +4,12 @@ interface Props {
   type: "submit" | "button" | "reset";
   children: ReactNode;
   className?: string;
+  onClick?: () => void;
 }
 
-export default function Button({ className, type, children }: Props) {
+export default function Button({ className, type, children, onClick }: Props) {
   return (
-    <button className={className} type={type}>
+    <button className={className} type={type} onClick={onClick}>
       {children}
     </button>
   );
