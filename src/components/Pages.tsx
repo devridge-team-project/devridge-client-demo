c0691450bc02736961523323626f65a7fd2ba830
jyj1111 <jyj202040@gmail.com>
2024-01-22 18:09:36 +0900

    Feat: help-pw routing

--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./home/Page";
 import SignInPage from "./sign-in/Page";
+import HelpPwPage from "./help-pw/Page";
 import { signUpRoutes } from "./sign-up/controller/Routes";
 
 export default function Pages() {
@@ -10,6 +11,7 @@ export default function Pages() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/sign-in" element={<SignInPage />} />
+      <Route path="/help-pw" element={<HelpPwPage />} />
       {routers.map(([path, element]) => (
         <Route key={path} path={path} element={element} />
       ))}
