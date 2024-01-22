40968d0f49183dda9f84201b2e9c326d7c8b578a
jyj1111 <jyj202040@gmail.com>
2024-01-22 18:06:37 +0900

    Feat: home authorization check

--- src/api/sign-in/loginService.ts
+++ src/api/sign-in/loginService.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError } from "axios";
-
+import { setCookie, removeCookie } from "util/cookies";
 import { LoginResponse, LoginRequest } from "../../interface/Login";
 import { axiosJsonInstance } from "../axios";
 
@@ -11,8 +11,8 @@ export const login = async (user: LoginRequest) => {
     } = data;
     console.log(data);
     console.log(accessToken);
-
-    // axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+    const expiration = new Date(Date.now() + 900 * 1000);
+    setCookie("accessToken", accessToken, { expires: expiration });
     return { status, accessToken };
   } catch (error) {
     if (error instanceof AxiosError && error.response?.status) {
@@ -25,7 +25,27 @@ export const login = async (user: LoginRequest) => {
 
 export const logout = async () => {
   try {
-    const { status } = await axiosJsonInstance.get("/logout");
+    const { status } = await axiosJsonInstance.post("/logout");
+    removeCookie("accessToken");
+    return status;
+  } catch (error) {
+    if (error instanceof AxiosError && error.response?.status) {
+      const { status } = error.response;
+      return status;
+    }
+    return 500;
+  }
+};
+
+export const authTest = async () => {
+  try {
+    const { status, data } = await axiosJsonInstance.get("/users/test");
+    console.log(status, data);
+    const {
+      body: { accessToken },
+    } = data;
+    const expiration = new Date(Date.now() + 900 * 1000);
+    setCookie("accessToken", accessToken, { expires: expiration });
     return status;
   } catch (error) {
     if (error instanceof AxiosError && error.response?.status) {
