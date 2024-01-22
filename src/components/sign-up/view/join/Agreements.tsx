01e54c324761ad53e2b93bd60a04d8d57dcaf738
Park Minhyeong <duncansITworld@gmail.com>
2024-01-23 00:29:14 +0900

    [Feat] add agreements click event

--- src/components/sign-up/view/join/Agreements.tsx
+++ src/components/sign-up/view/join/Agreements.tsx
@@ -1,25 +1,29 @@
 import { useState } from "react";
 import LineBreak from "design/LineBreak";
-import { agreements } from "asset/sign-up/agreements";
+import { agreements as agreementsScripts } from "asset/sign-up/agreements";
 import { between, col, row } from "style/display";
 import { useWidgetStore } from "shared/store";
 import SignUpLayout from "design/layout/sign-up/SignUpLayout";
+import { useSignUpStore } from "shared/sign-up/store";
 
 export default function Agreements() {
   const { setView } = useWidgetStore();
+  const { agreements, setAgreement } = useSignUpStore();
+  const agreementsArray = Object.entries(agreements);
+
   return (
     <SignUpLayout
       titles={["약관 동의가", "필요해요"]}
       buttons={[["다음으로", () => setView("credentials")]]}
     >
       <div className={col(5)}>
-        {agreements.map(({ title, contents }) => (
-          <Agreement title={title} contents={contents} key={title} />
+        {agreementsScripts.map(({ title, contents }, index) => (
+          <Agreement key={title} title={title} contents={contents} flag={agreementsArray[index]} />
         ))}
       </div>
       <div className={`${col(5)} w-full `}>
         <div className={`${row(4)} rounded-md border-2 px-6 py-3`}>
-          <button className="h-6 w-6 border-2" />
+          <button className="h-6 w-6 border-2" onClick={() => setAgreement("flag6")} />
           <div>만 14세 이상입니다.</div>
         </div>
       </div>
@@ -27,13 +31,30 @@ export default function Agreements() {
   );
 }
 
-function Agreement({ title, contents }: { title: string; contents: string[] | null }) {
+function Agreement({
+  title,
+  contents,
+  flag,
+}: {
+  title: string;
+  contents: string[] | null;
+  flag: [string, boolean];
+}) {
+  const { setAgreement, setAllAgreements } = useSignUpStore();
   const [open, setOpen] = useState(false);
+  const [key, value] = flag;
+
   return (
     <div>
       <div className={`${between.row} text-lg`}>
         <div className={`${row(2)} items-center`}>
-          <button className="h-6 w-6 border-2" />
+          <button
+            className={`h-6 w-6 border-2 ${value ? "bg-blue-500" : ""}`}
+            onClick={() => {
+              if (key === "all") return setAllAgreements();
+              return setAgreement(key);
+            }}
+          />
           <div>{title}</div>
         </div>
         {contents ? <button onClick={() => setOpen(!open)}>{">"}</button> : null}
