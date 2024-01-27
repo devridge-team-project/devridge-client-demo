28cc4dcb87b3f38c21be91424ee9f71a8430347e
jyj1111 <jyj202040@gmail.com>
2024-01-28 03:17:28 +0900

    [Feat] mypage routing

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,12 +1,24 @@
 import { Home, Join, Qna, SignIn, SignUp, Success } from "components";
 import { Route, Routes } from "react-router-dom";
 
+import HelpPwPage from "components/help-pw/Page";
+import MyAccountPage from "components/myAccount/myAccountPage";
+import DeleteAccountPage from "components/myAccount/deleteAccountPage";
+import UpdateAccountPage from "components/myAccount/updateAccountPage";
+import ChangePasswordPage from "components/myAccount/changePasswordPage";
+import QuestionPage from "components/myAccount/questionPage";
+
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
-      {/* <Route path="/help-pw" element={<Help />} /> */}
+      <Route path="/help-pw" element={<HelpPwPage />} />
+      <Route path="/mypage" element={<MyAccountPage />} />
+      <Route path="/delete-account" element={<DeleteAccountPage />} />
+      <Route path="/update-account" element={<UpdateAccountPage />} />
+      <Route path="/change-pw" element={<ChangePasswordPage />} />
+      <Route path="/ask" element={<QuestionPage />} />
       <Route path="/sign-up">
         <Route index element={<SignUp />} />
         <Route path="join" element={<Join />} />
