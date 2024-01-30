b1e2599457dbd6831bc2a6110e7b0eff53dcb852
Park Minhyeong <duncansITworld@gmail.com>
2024-01-30 20:50:17 +0900

    [Chore] not proxy

--- src/connection/remotes.ts
+++ src/connection/remotes.ts
@@ -1,47 +1,14 @@
+import { Qna, QnaDetail } from "interface/Qna";
 import httpRequest from "./http";
 
-interface Member {
-  id: number;
-  nickname: string;
-  profileImageUrl: string | null;
-  introduction: string | null;
-}
-
-interface Qna {
-  id: number;
-  title: string;
-  likes: number;
-  views: number;
-  commentCount: number;
-}
-
-interface QnaComments {
-  id: number;
-  member: Member;
-  content: string;
-  likes: number;
-  dislikes: number;
-  createdAt: string;
-}
-
-interface QnaDetail {
-  member: Member;
-  title: string;
-  content: string;
-  views: number;
-  likes: number;
-  dislikes: number;
-  createdAt: string;
-  commentCount: number;
-  comments: QnaComments[];
-}
+const { REACT_APP_SERVER_URL: origin } = process.env;
 
 function getQna() {
-  return httpRequest.get<Qna[]>("/api/qna?sortOption=views");
+  return httpRequest.get<Qna[]>(`${origin}/api/qna?sortOption=views`);
 }
 
 function getQnaById(id: number) {
-  return httpRequest.get<QnaDetail>(`/api/qna/${id}`);
+  return httpRequest.get<QnaDetail>(`${origin}/api/qna/${id}`);
 }
 
 export const qna = {
---
+++ src/interface/Qna.ts
@@ -0,0 +1,35 @@
+export interface Member {
+  id: number;
+  nickname: string;
+  profileImageUrl: string | null;
+  introduction: string | null;
+}
+
+export interface Qna {
+  id: number;
+  title: string;
+  likes: number;
+  views: number;
+  commentCount: number;
+}
+
+export interface QnaComments {
+  id: number;
+  member: Member;
+  content: string;
+  likes: number;
+  dislikes: number;
+  createdAt: string;
+}
+
+export interface QnaDetail {
+  member: Member;
+  title: string;
+  content: string;
+  views: number;
+  likes: number;
+  dislikes: number;
+  createdAt: string;
+  commentCount: number;
+  comments: QnaComments[];
+}
