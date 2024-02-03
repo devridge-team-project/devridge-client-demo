08122d4b394f613ac1dec5d5530273f656126fd1
jyj1111 <jyj202040@gmail.com>
2024-02-04 02:01:55 +0900

    [Feat] nickname, image ui 추가

--- src/components/myAccount/view/UpdateAccount.tsx
+++ src/components/myAccount/view/UpdateAccount.tsx
@@ -5,16 +5,26 @@ import Input from "../../common/input";
 import Button from "../../common/button";
 
 export default function UpdateAccount() {
-  const { nickname, profileImageUrl } = useSignUpStore();
+  const { nickname, profileImageUrl, occupation } = useSignUpStore();
   const onSubmitHandler = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
   return (
-    <div className={`min-h-screen ${center.colO(0)}`}>
+    <div className={`min-h-screen ${center.colO(0)} `}>
+      <div className={`w-80 text-1xl font-bold ${row(2)}`}>회원정보 수정</div>
+      <img
+        src={profileImageUrl}
+        className="mt-7.5 h-25 w-25 rounded-full bg-gray-200 "
+        alt="profileImage"
+      />
+      <div className="mt-7 text-2xl font-bold">{nickname}</div>
       <form className={`${col(2, 80)}`} onSubmit={onSubmitHandler}>
-        <div className="text-2xl font-bold">회원정보 수정</div>
-        <img src={profileImageUrl} alt="profileImage" />
-        <div>{nickname}</div>
+        <Input
+          className="mt-2.5 block h-14 w-80 border"
+          type="text"
+          name="occupation"
+          value={occupation}
+        />
         <Input
           className="mt-2.5 block h-14 w-80 border"
           type="text"
