0f4ef3d27d5003fcabe1fb409460b9afc85b7cfc
jyj1111 <jyj202040@gmail.com>
2024-02-04 02:00:46 +0900

    [Feat] image, nickname ui 추가

--- src/components/myAccount/view/MyAccount.tsx
+++ src/components/myAccount/view/MyAccount.tsx
@@ -1,13 +1,31 @@
 import { center, col, row } from "style/display";
 import { Link } from "react-router-dom";
-
+import { useSignUpStore } from "shared/sign-up/store";
 export default function MyAccount() {
+  const { nickname, setNickname, profileImageUrl, setProfileImageUrl, occupation, setOccupation } =
+    useSignUpStore();
   return (
     <div className={`min-h-screen ${center.colO(0)}`}>
-      <Link to="/update-account">회원정보 수정</Link>
-      <Link to="/change-pw">비밀번호 변경</Link>
-      <Link to="/delete-account">회원탈퇴</Link>
-      <Link to="/ask">문의사항</Link>
+      <img
+        src={profileImageUrl}
+        className="h-25 w-25 rounded-full bg-gray-200 "
+        alt="profileImage"
+      />
+      <div className="mt-7 text-2xl font-bold">{nickname}</div>
+      <div className="mt-3 text-1xl font-bold text-blue-600 ">{occupation}</div>
+      <div className={`mt-7.5 w-80 font-bold ${row(2)}`}>
+        <Link to="/update-account">회원정보 수정</Link>
+      </div>
+      <div className={`mt-7.5 w-80 font-bold ${row(2)}`}>
+        {" "}
+        <Link to="/change-pw">비밀번호 변경</Link>
+      </div>
+      <div className={`mt-7.5 w-80 font-bold ${row(2)}`}>
+        <Link to="/delete-account">회원탈퇴</Link>
+      </div>
+      <div className={`mt-7.5 w-80 font-bold ${row(2)}`}>
+        <Link to="/ask">문의사항</Link>
+      </div>
     </div>
   );
 }
