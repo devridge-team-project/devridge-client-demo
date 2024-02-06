0c98fe2c8575295276c7efa58daa857b362686b5
jyj1111 <jyj202040@gmail.com>
2024-02-06 22:15:11 +0900

    [Feat] help-pw email add

--- src/components/help-pw/view/HelpPw.tsx
+++ src/components/help-pw/view/HelpPw.tsx
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { center, col, row } from "style/display";
-
+import { emailAuth } from "api/sign-in/loginService";
+import { useNavigate } from "react-router-dom";
 import Input from "../../common/input";
 import Button from "../../common/button";
 
 export default function HelpPw() {
   const [email, setEmail] = useState("");
+  const navigate = useNavigate();
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setEmail(value);
   };
-  const onSubmitHandler = () => {};
+  const onSubmitHandler = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const status = await emailAuth({ email });
+    if (status === 200) {
+      navigate("/help-pw/email-auth", { state: { email } });
+    }
+  };
   return (
     <div className={`min-h-screen ${center.colO(0)}`}>
       <form className={`${col(2, 80)}`} onSubmit={onSubmitHandler}>
@@ -24,7 +32,7 @@ export default function HelpPw() {
           onChange={onChange}
         />
         <Button className="mt-5 h-14 w-80  bg-black text-white" type="submit">
-          비밀번호 재설정 이메일 받기
+          비밀번호 재설정 이메일 인증번호 받기
         </Button>
       </form>
     </div>
