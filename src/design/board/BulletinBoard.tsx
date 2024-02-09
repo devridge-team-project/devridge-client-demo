724dcfb1ca3503b5ed2430a4d027efb0612548b3
minhyeong <minhyeong.dev@gmail.com>
2024-02-09 18:18:05 +0900

    [Feat] add comment section in bulletinboard

--- src/design/board/BulletinBoard.tsx
+++ src/design/board/BulletinBoard.tsx
@@ -1,3 +1,4 @@
+import { QnaComments } from "interface/Qna";
 import Board from "./Board";
 
 export default function BulletinBoard({
@@ -5,11 +6,13 @@ export default function BulletinBoard({
   title,
   createdAt,
   content,
+  comments,
 }: {
   type: string;
   title: string;
   createdAt: string;
   content: string;
+  comments: QnaComments[];
 }) {
   return (
     <Board>
@@ -21,6 +24,25 @@ export default function BulletinBoard({
         <div className="text-sm text-gray-200">{createdAt}</div>
       </div>
       <div className="text-xl">{content}</div>
+      <div className="flex flex-col gap-4">
+        <div className="text-2xl font-bold">댓글 {`(${comments.length})`}</div>
+        {comments.map(({ member: { nickname, profileImageUrl, introduction }, content }) => (
+          <div className="rounded-xl border-2 p-4 flex flex-col gap-4 min-h-80">
+            <div className="flex items-center border-b pb-8 gap-4">
+              {profileImageUrl ? (
+                <img src={profileImageUrl} alt="profile" className="rounded-full size-16 " />
+              ) : (
+                <div className="rounded-full bg-black size-16" />
+              )}
+              <div>
+                <div className="text-lg font-bold">{nickname}</div>
+                <div className="text-sm text-gray-300">{introduction ?? "지나가는 개발자"}</div>
+              </div>
+            </div>
+            <div>{content}</div>
+          </div>
+        ))}
+      </div>
     </Board>
   );
 }
--- src/design/template/Header.tsx
+++ src/design/template/Header.tsx
@@ -1,16 +1,25 @@
 import { useWidgetsStore } from "shared/store";
 import { cn } from "util/classNames";
+import useNavigation from "hook/useNavigation";
 
 export default function Header() {
-  const { setWidget } = useWidgetsStore();
-  const positions = "flex justify-end items-center";
+  const { setWidget, clearWidget } = useWidgetsStore();
+  const navigate = useNavigation();
+
+  const positions = "flex justify-between items-center";
   const displays = "fixed top-0 left-0 z-50";
   const sizes = "w-full h-20";
-  const paddings = "pr-6";
+  const paddings = "px-6";
   const styles = "bg-white";
   return (
     <>
       <div className={cn(sizes, displays, styles, positions, paddings)}>
+        <div
+          onClick={() => navigate("/", clearWidget)}
+          className="text-2xl font-bold cursor-pointer"
+        >
+          DEVRIDGE
+        </div>
         <button onClick={() => setWidget("sideMenu")}>
           <img src="/images/icons/hamburger-bar.svg" alt="hamburger-bar" className="size-7" />
         </button>
