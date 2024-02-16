a682b477e156b9f7da0d9ebd027c68589bb63c44
minhyeong <minhyeong.dev@gmail.com>
2024-02-16 22:13:41 +0900

    [Style] fix style

--- src/design/index.ts
+++ src/design/index.ts
@@ -1,4 +1,5 @@
 export * from "./button";
-export { default as Input } from "./input/Input";
+export { default as BulletinBoard } from "./board/BulletinBoard";
 export { default as Card } from "./card/Card";
+export { default as Input } from "./input/Input";
 export { default as TextArea } from "./input/TextArea";
