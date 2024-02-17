1e5a2801ee9808c82e0b95a37fdc1c1eceb79517
jyj1111 <jyj202040@gmail.com>
2024-02-18 04:05:21 +0900

    Refator]: updateAccount page tanstack-qeury

--- src/components/myAccount/view/UpdateAccount.tsx
+++ src/components/myAccount/view/UpdateAccount.tsx
@@ -1,35 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { center, col, row } from "style/display";
 import { useSignUpStore } from "shared/sign-up/store";
-import { skillInfos } from "connection/api/myInfoService";
+import { skills } from "connection/api/skills";
 import Input from "../../common/input";
-
+import { useQuery } from "@tanstack/react-query";
 export default function UpdateAccount() {
-  const [skills, setSkills] = useState<string[]>([]);
+  const [skill, setSkill] = useState<string[]>([]);
   const { nickname, profileImageUrl, occupation, skillIds } = useSignUpStore();
   const onSubmitHandler = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
-  const getSkills = async () => {
-    const { status, data } = await skillInfos();
-    console.log(skillIds);
-    console.log(
-      skillIds.map((skillId: number) => {
-        return data[skillId - 1].skillName;
-      }),
-    );
-    if (status === 200 && skillIds) {
-      setSkills([
+  const { data: skillInfo } = useQuery({ queryKey: ["skill"], queryFn: () => skills.get() });
+  useEffect(() => {
+    if (skillInfo && skillIds) {
+      setSkill([
         ...skillIds.map((skillId: number) => {
-          return data[skillId - 1].skillName;
+          return skillInfo[skillId - 1].skillName;
         }),
       ]);
     }
-  };
-  useEffect(() => {
-    getSkills();
-  }, []);
-  console.log(skills);
+  }, [skillInfo]);
+
   return (
     <div className={`min-h-screen ${center.colO(0)} `}>
       <div className={`w-80 text-1xl font-bold ${row(2)}`}>회원정보 수정</div>
@@ -53,7 +44,7 @@ export default function UpdateAccount() {
           placeholder="스킬을 검색해보세요"
         />
         <div className="flex max-h-84 w-full flex-wrap gap-2 overflow-hidden">
-          {skills.map((skill) => (
+          {skill.map((skill) => (
             <button className="bg-blue-500 border-blue-500 text-white font-bold flex h-10 grow items-center justify-center rounded-full border-2 px-4 duration-500">
               {skill}
             </button>
