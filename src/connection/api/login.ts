dd9265ffb682a96d3b7822048635a9454d1b909b
jyj1111 <jyj202040@gmail.com>
2024-02-18 03:56:46 +0900

    [Refactor] login api refactor

--- src/connection/api/login.ts
+++ src/connection/api/login.ts
@@ -11,7 +11,7 @@ export const login = async (user: LoginRequest) => {
     const { accessToken } = data;
 
     console.log(accessToken);
-    const expiration = new Date(Date.now() + 900 * 1000);
+    const expiration = new Date(Date.now() + 12 * 60 * 60 * 1000);
     setCookie("accessToken", accessToken, { expires: expiration });
 
     return { status: 200, accessToken };
@@ -39,65 +39,11 @@ export const logout = async () => {
   }
 };
 
-export const authTest = async () => {
-  try {
-    const res = await axiosJsonInstance.get("api/users/test");
-    console.log(res);
-    const { status, data } = await axiosJsonInstance.get("api/users/test");
-
-    console.log(status, data);
-    const {
-      body: { accessToken },
-    } = data;
-    const expiration = new Date(Date.now() + 900 * 1000);
-    setCookie("accessToken", accessToken, { expires: expiration });
-    return status;
-  } catch (error) {
-    if (error instanceof AxiosError && error.response?.status) {
-      const { status } = error.response;
-      return status;
-    }
-    return 500;
-  }
-};
-
 export const deleteAccount = async (password: PasswordRequest) => {
   console.log(password);
   try {
     const { status, data } = await axiosJsonInstance.delete("api/users", { data: password });
     removeCookie("accessToken");
-    console.log(status);
-    return status;
-  } catch (error) {
-    if (error instanceof AxiosError && error.response?.status) {
-      const { status } = error.response;
-      return status;
-    }
-    return 500;
-  }
-};
-
-export const emailAuth = async (email: EmailRequest) => {
-  console.log(email);
-  try {
-    const { status } = await axiosJsonInstance.post("api/email-verifications", email);
-    console.log(status);
-    return status;
-  } catch (error) {
-    if (error instanceof AxiosError && error.response?.status) {
-      const { status } = error.response;
-      return status;
-    }
-    return 500;
-  }
-};
-
-export const codeCheck = async ({ email, code }: CodeRequest) => {
-  console.log(email, code);
-  try {
-    const { status } = await axiosJsonInstance.get(
-      `api/email-verifications?email=${email}&code=${code}`,
-    );
     console.log(status);
     return status;
   } catch (error) {
