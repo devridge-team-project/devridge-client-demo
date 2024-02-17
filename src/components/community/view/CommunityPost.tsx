2e185b37e8f6d813431148deb42836091b85559d
jyj1111 <jyj202040@gmail.com>
2024-02-18 04:03:31 +0900

    [Refactor] communitypost page tanstack-query

--- src/components/community/view/CommunityPost.tsx
+++ src/components/community/view/CommunityPost.tsx
@@ -4,36 +4,33 @@ import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { Button, Input, TextArea } from "design";
-import { CommunityWrite } from "connection/api/communityService";
+import { postCommunity } from "connection/api/community";
 
 export default function CommunityPost() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [hashtags, setHashtags] = useState([]);
   const navigate = useNavigate();
-
-  const onSubmitHandler = async (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const status = await CommunityWrite({ title, content, hashtags });
-    if (status === 201) {
-      alert("커뮤니티 게시글 등록 성공");
-      navigate("/community");
-    }
-  };
+  const { mutate, isSuccess } = useMutation({
+    mutationKey: ["postCommunity"],
+    mutationFn: () => postCommunity({ title, content, hashtags }),
+  });
+  if (isSuccess) {
+    alert("커뮤니티 게시글 등록 성공");
+    navigate("/community");
+  }
 
   return (
     <Board options={{ gapY: 8 }}>
-      <form onSubmit={onSubmitHandler}>
-        <div>
-          <div className="text-2xl font-bold">제목</div>
-          <Input onChange={[title, setTitle]} placeholder="제목을 5자 이상 입력해주세요." />
-        </div>
-        <div>
-          <div className="text-2xl font-bold">내용</div>
-          <TextArea onChange={[content, setContent]} placeholder="궁금한 것을 작성해보세요." />
-        </div>
-        <Button title="작성하기" type="submit" options={{ size: "full" }} />
-      </form>
+      <div>
+        <div className="text-2xl font-bold">제목</div>
+        <Input onChange={[title, setTitle]} placeholder="제목을 5자 이상 입력해주세요." />
+      </div>
+      <div>
+        <div className="text-2xl font-bold">내용</div>
+        <TextArea onChange={[content, setContent]} placeholder="궁금한 것을 작성해보세요." />
+      </div>
+      <Button title="작성하기" onClick={mutate} options={{ size: "full" }} />
     </Board>
   );
 }
