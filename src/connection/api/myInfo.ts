db2b7b8627ca023fd0ddaeca2f6322ca6ad5523b
jyj1111 <jyj202040@gmail.com>
2024-02-19 00:44:03 +0900

    [Rename] myinfo->password

--- src/connection/api/myInfo.ts
+++
@@ -1,6 +0,0 @@
-import httpRequest from "../axios";
-import { PasswordRequest } from "../../interface/Login";
-
-export const changePassword = (password: PasswordRequest) => {
-  return httpRequest.patch("api/users/password", password);
-};
---
+++ src/connection/api/password.ts
@@ -0,0 +1,9 @@
+import httpRequest from "../axios";
+
+export const changePassword = (password: string) => {
+  return httpRequest.patch("api/users/password", { password });
+};
+
+export const resetPassword = (password: string, email: string) => {
+  return httpRequest.patch("api/users/reset-password", { password, email });
+};
