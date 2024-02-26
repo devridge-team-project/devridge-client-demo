68ea7cbd098e462e16e429df696f6c6b7d30eada
jyj1111 <jyj202040@gmail.com>
2024-02-26 18:32:03 +0900

    [Feat] footer layout styling

--- src/design/template/Footer.tsx
+++ src/design/template/Footer.tsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="h-[250px] bg-black">
+      <div className="flex justify-center pt-40">
+        <Link to="https://github.com/">
+          <img src="/images/icons/github.png" alt="gitHub" className="w-7.5 h-7.5 mr-2.5" />
+        </Link>
+        <Link to="https://www.instagram.com/">
+          <img src="/images/icons/instagram.png" alt="instagram" className="w-7.5 h-7.5 mr-2.5" />
+        </Link>
+        <Link to="https://kr.linkedin.com/">
+          <img src="/images/icons/linkedIn.png" alt="linkedIn" className="w-7.5 h-7.5 mr-2.5" />
+        </Link>
+      </div>
+      <div className="font-bold flex justify-center pt-1 text-white">
+        © 2024 DEVRIDGE, All rights reserved. di
+      </div>
+    </div>
+  );
 }
