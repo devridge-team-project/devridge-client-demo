16e95e658f07595cb31849a42ca14a280b7dd3d2
jyj1111 <jyj202040@gmail.com>
2024-03-10 04:48:16 +0900

    [Refactor] select button refactor

--- src/design/input/SelectButton.tsx
+++ src/design/input/SelectButton.tsx
@@ -2,18 +2,27 @@ import React from "react";
 
 interface Radio {
   id: string;
+  type: string;
   name: string;
-  defaultValue: string;
+  defaultValue?: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
   className?: string;
 }
 
-export default function RadioButton({ id, name, defaultValue, value, onChange, className }: Radio) {
+export default function SelectButton({
+  id,
+  type,
+  name,
+  defaultValue,
+  value,
+  onChange,
+  className,
+}: Radio) {
   return (
     <label htmlFor={id} className={`block text-center mr-2.5 ${className}`}>
       <input
-        type="radio"
+        type={type}
         id={id}
         name={name}
         value={value}
