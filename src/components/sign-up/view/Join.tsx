6d95e1874633c758ab7c1da672334ad4dce7a4bf
minhyeong <minhyeong.dev@gmail.com>
2024-03-09 21:31:25 +0900

    [Feat] exception for sign in views

--- src/components/sign-up/view/Join.tsx
+++ src/components/sign-up/view/Join.tsx
@@ -1,15 +1,30 @@
-import { center } from "style/display";
 import Agreements from "./join/Agreements";
 import { useSearchParams } from "react-router-dom";
 import { Events } from "design";
+import { useWidgetStore } from "shared";
+import { center } from "style/display";
+import { cn } from "util/classNames";
+import PersonalInformation from "./join/PersonalInformation";
+import Credentials from "./join/Credentials";
 
 export default function Join() {
   const [searchParams, setSearchParams] = useSearchParams();
   const code = searchParams.get("code");
 
+  const conatiner = {
+    positions: "absolute top-0 ",
+    displays: "flex flex-col items-center",
+    sizes: "w-full min-h-screen ",
+  };
+
   return (
-    <div className={`absolute top-0 w-full min-h-screen ${center.colO(0)}`}>
-      <Events.Replace exceptions={[[code, <div>gd</div>]]}>
+    <div className={cn(conatiner)}>
+      <Events.Replace
+        exceptions={[
+          [code || "personalInformation", <PersonalInformation />],
+          ["credentials", <Credentials />],
+        ]}
+      >
         <Agreements />
       </Events.Replace>
     </div>
