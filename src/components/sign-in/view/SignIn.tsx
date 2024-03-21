507c06cfaa64294cd56d8d91e5998c5a03323745
jyj1111 <jyj202040@gmail.com>
2024-03-21 23:36:32 +0900

    [Refactor]: login 시 usemutation 사용

--- src/components/sign-in/view/SignIn.tsx
+++ src/components/sign-in/view/SignIn.tsx
@@ -2,39 +2,40 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { center, col, row } from "style/display";
 import { contents } from "../static/platform";
+import { useMutation } from "@tanstack/react-query";
 import { login } from "connection/api/login";
-import { useAuthStore } from "shared/auth/store";
+import { setCookie } from "util/cookies";
+import { useSignUpStore } from "shared/sign-up/store";
 import Input from "../../common/input";
+import { Button } from "design";
 
 export default function SignIn() {
   const [info, setInfo] = useState({ email: "", password: "" });
   const navigate = useNavigate();
-  const { setAccessToken, setSignInType } = useAuthStore();
+  // const { authToken, setAuthToken } = useSignUpStore();
   const { email, password } = info;
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setInfo({ ...info, [name]: value });
   };
-  const onSubmitHandler = async (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const { status, accessToken } = await login({ email, password });
+  const { data, mutate, isSuccess, isError } = useMutation({
+    mutationFn: () => login({ email, password }),
+  });
+  if (isSuccess && data) {
+    console.log(data);
+    const expiration = new Date(Date.now() + 20 * 60 * 1000);
+    // setAuthToken(data.accessToken);
+    setCookie("accessToken", data.accessToken, { expires: expiration });
+    alert("로그인 되었습니다.");
+    navigate("/");
+  } else if (isError) {
+    alert("이메일 또는 비밀번호가 틀렸습니다.");
+  }
+  // console.log(authToken);
 
-    if (status === 200) {
-      setAccessToken(accessToken);
-      setSignInType("GeneralLogin");
-
-      alert("로그인 되었습니다.");
-      navigate("/");
-    } else if (status === 400) {
-      alert("이메일 또는 비밀번호가 틀렸습니다.");
-    } else if (status === 401) {
-      alert("이메일 또는 비밀번호가 틀렸습니다.");
-    }
-    setInfo({ email: "", password: "" });
-  };
   return (
     <div className={`absolute top-0 w-full min-h-screen ${center.colO(0)}`}>
-      <form className={`${col(2, 80)} `} onSubmit={onSubmitHandler}>
+      <div className={`${col(2, 80)} `}>
         <div className="text-2xl font-bold text-blue-600">로그인</div>
         <Input
           className="mt-2.5 block h-14 w-80 border"
@@ -52,15 +53,14 @@ export default function SignIn() {
           placeholder="비밀번호를 입력해주세요"
           onChange={onChange}
         />
-        <button className="mt-5 h-14 w-80  bg-black text-white" type="submit">
-          로그인
-        </button>
+        <Button title="로그인" onClick={mutate} options={{ size: "full" }} />
+
         <div className="mt-2 flex justify-end ">
           <Link to="/help-pw" className=" block  text-xs text-gray-500 ">
             비밀번호 찾기
           </Link>
         </div>
-      </form>
+      </div>
       <div className="flex flex-col items-center gap-4 text-sm text-gray-500 font-bold">
         <div>다른 계정으로 로그인하기</div>
         <div className={`mx-8 mt-2 ${row(5)}`}>
