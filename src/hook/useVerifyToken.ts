5755034ef1748c2eb7323b17cda996aa07da26f5
jyj1111 <jyj202040@gmail.com>
2024-03-21 23:39:22 +0900

    [Feat] 토큰 유효성 판단 토큰 훅

---
+++ src/hook/useVerifyToken.ts
@@ -0,0 +1,40 @@
+import { getCookie, setCookie, removeCookie } from "util/cookies";
+import { useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useSignUpStore } from "shared";
+import { refresh } from "connection/api/login";
+import { useQuery } from "@tanstack/react-query";
+
+export default function useVerifyToken(key: string) {
+  const token = getCookie("accessToken") as string;
+  const [isValidToken, setIsValidToken] = useState<boolean>(false);
+  const { setAuthToken } = useSignUpStore();
+  const { data, isSuccess, isError } = useQuery({
+    queryKey: ["refresh"],
+    queryFn: () => refresh(),
+  });
+  console.log(token);
+  useEffect(() => {
+    if (token) {
+      const { exp } = jwtDecode(token);
+      if ((exp as number) <= Math.floor(Date.now() / 1000)) {
+        if (isSuccess) {
+          const expiration = new Date(Date.now() + 20 * 60 * 1000);
+          removeCookie("accessToken");
+          setCookie("accessToken", data?.accessToken, { expires: expiration });
+          setAuthToken(data?.accessToken);
+          setIsValidToken(true);
+        }
+        if (isError) {
+          console.log(isError);
+          setIsValidToken(false);
+        }
+      }
+      setIsValidToken(true);
+    } else {
+      setIsValidToken(false);
+    }
+  }, [token, key, setAuthToken]);
+
+  return isValidToken;
+}
