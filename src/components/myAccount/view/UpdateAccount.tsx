506db689b6be7531b0558825868610e7e915c377
jyj1111 <jyj202040@gmail.com>
2024-03-28 00:11:10 +0900

    [Feat] user 수정시 skill 수정 추가

--- src/components/myAccount/view/UpdateAccount.tsx
+++ src/components/myAccount/view/UpdateAccount.tsx
@@ -1,20 +1,67 @@
 import React, { useState, useEffect } from "react";
 import { center, col, row } from "style/display";
 import { skills } from "connection/api/skills";
+import { useSignUpStore } from "shared/sign-up/store";
 import Input from "../../common/input";
-import { useQuery } from "@tanstack/react-query";
+import { Button } from "design";
+import { Skill } from "interface/Skill";
+import { users } from "connection";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 export default function UpdateAccount() {
+  const {
+    signUpData: { nickname, occupation, introduction, imageUrl, skillIds },
+  } = useSignUpStore();
   const [skill, setSkill] = useState<string[]>([]);
-  const [nickname, setNickname] = useState<string>("");
-  const [occupation, setOccupation] = useState<string>("");
-  const [profileImageUrl, setProfileImageUrl] = useState<string>("");
-  const skillIds = [1, 2, 3];
-
-  const onSubmitHandler = async (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+  const [job, setJob] = useState<string>(occupation);
+  const [profileImageUrl, setProfileImageUrl] = useState<string | null>(imageUrl);
+  const [searchWord, setSearchWord] = useState<string>("");
+  const [searchItems, setSearchItems] = useState<Skill[]>([]);
   const { data: skillInfo } = useQuery({ queryKey: ["skill"], queryFn: () => skills.get() });
+
+  const data = {
+    skillIds: skill.map((skillName) => {
+      const matchedSkill = skillInfo?.find((skill) => skill.skillName === skillName);
+      return matchedSkill?.id;
+    }),
+    introduction: introduction,
+  };
+  const formData = new FormData();
+  const blob = new Blob([JSON.stringify(data)], { type: "application/json" });
+  const file = new File([""], "/images/test/default.png");
+  formData.append("image", file);
+  formData.append("info", blob);
+
+  const { mutate, data: res } = useMutation({
+    mutationFn: () => users.patch(formData),
+  });
+
+  console.log(res);
+
+  const onSearchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchWord(e.target.value);
+  };
+
+  const onJobHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setJob(e.target.value);
+  };
+
+  const onAddSkillHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    const set = new Set(skill);
+    const target = e.target as HTMLDivElement;
+    console.log(target.dataset.skillName);
+    set.add(target.dataset.skillName as string);
+    setSkill([...set]);
+  };
+
+  useEffect(() => {
+    if (skillInfo) {
+      setSearchItems([
+        ...skillInfo.filter(({ id, skillName }) => skillName.includes(searchWord) === true),
+      ]);
+    }
+  }, [searchWord]);
+
   useEffect(() => {
     if (skillInfo && skillIds) {
       setSkill([
@@ -29,35 +76,48 @@ export default function UpdateAccount() {
     <div className={`min-h-screen ${center.colO(0)} `}>
       <div className={`w-80 text-1xl text-blue-grey font-bold ${row(2)}`}>회원정보 수정</div>
       <img
-        src={profileImageUrl}
+        src={profileImageUrl as string}
         className="mt-7.5 h-25 w-25 rounded-full bg-gray-200 "
         alt="profileImage"
       />
       <div className="mt-7 text-2xl text-blue-grey font-bold">{nickname}</div>
-      <form className={`${col(2, 80)}`} onSubmit={onSubmitHandler}>
+      <div className={`${col(2, 80)}`}>
+        <div className="font-bold text-xs">자기소개(필수)</div>
         <Input
-          className="mt-2.5 block h-14 w-80 border"
+          className="mt-1.5 block h-14 w-80 border"
           type="text"
-          name="occupation"
-          value={occupation}
+          name="job"
+          onChange={onJobHandler}
+          value={job}
         />
+        <div className=" font-bold text-xs mt-5">보유 스킬</div>
         <Input
-          className="mt-2.5 block h-14 w-80 border"
+          className="mt-1.5 block h-14 w-80 border"
           type="text"
           name="search-skills"
           placeholder="스킬을 검색해보세요"
+          onChange={onSearchHandler}
+          value={searchWord}
         />
-        <div className="flex max-h-84 w-full flex-wrap gap-2 overflow-hidden">
+        {searchWord && (
+          <div className="max-h-40 ">
+            {searchItems.map(({ id, skillName }) => (
+              <div onClick={onAddSkillHandler} data-skill-name={skillName}>
+                {skillName}
+              </div>
+            ))}
+          </div>
+        )}
+
+        <div className="flex  max-h-84 w-full flex-wrap gap-2 overflow-hidden">
           {skill.map((skill) => (
             <button className="bg-blue-500 border-blue-500 text-white font-bold flex h-10 grow items-center justify-center rounded-full border-2 px-4 duration-500">
               {skill}
             </button>
           ))}
         </div>
-        <button className="mt-5 h-14 w-80  bg-black text-white" type="submit">
-          저장하기
-        </button>
-      </form>
+        <Button title="저장하기" onClick={mutate} options={{ size: "full" }} />
+      </div>
     </div>
   );
 }
--- src/connection/api/users.ts
+++ src/connection/api/users.ts
@@ -15,7 +15,12 @@ function postUsersV2(data: FormData) {
   return api.postMultipart("/api/users", data);
 }
 
+function patchUsers(data: FormData) {
+  return api.patchMultipart("/api/users", data);
+}
+
 export const users = {
   getDetails: getUserDetails,
   post: postUsersV2,
+  patch: patchUsers,
 };
