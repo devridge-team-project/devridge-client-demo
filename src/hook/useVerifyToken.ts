0858df00490d6baec5021ce099e1c83c3666913e
jyj1111 <jyj202040@gmail.com>
2024-03-30 01:57:31 +0900

    [Feat] useverifyToken 로직 변경

--- src/hook/useVerifyToken.ts
+++ src/hook/useVerifyToken.ts
@@ -1,42 +1,39 @@
-import { getCookie, setCookie, removeCookie } from "util/cookies";
-import { useState, useEffect } from "react";
+import { setCookie } from "util/cookies";
+import { useState, useEffect, useRef } from "react";
 import { jwtDecode } from "jwt-decode";
 import { useSignUpStore } from "shared";
 import { refresh } from "connection/api/login";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useVerifyToken(key: string) {
-  const token = getCookie("accessToken") as string;
-  const [isValidToken, setIsValidToken] = useState<boolean>(false);
-  const { setAuthToken } = useSignUpStore();
+  const isValidRef = useRef<boolean>(false);
+  const { authToken, setAuthToken } = useSignUpStore();
+
+  const timeOutFlag = !!(
+    authToken && (jwtDecode(authToken).exp as number) <= Math.floor(Date.now() / 1000)
+  );
   const { data, isSuccess, isError } = useQuery({
     queryKey: ["refresh"],
     queryFn: () => refresh(),
+    enabled: timeOutFlag,
   });
 
-  console.log(token);
   useEffect(() => {
-    if (token) {
-      const { exp } = jwtDecode(token);
-      const timeOutFlag = (exp as number) <= Math.floor(Date.now() / 1000);
+    if (authToken) {
       if (timeOutFlag) {
         if (isSuccess) {
-          const expiration = new Date(Date.now() + 20 * 60 * 1000);
-          removeCookie("accessToken");
+          const expiration = new Date(Date.now() + 12 * 60 * 60 * 1000);
           setCookie("accessToken", data?.accessToken, { expires: expiration });
           setAuthToken(data?.accessToken);
-          setIsValidToken(true);
-        }
-        if (isError) {
+          isValidRef.current = true;
+        } else if (isError) {
           console.log(isError);
-          setIsValidToken(false);
         }
+      } else {
+        isValidRef.current = true;
       }
-      setIsValidToken(true);
-    } else {
-      setIsValidToken(false);
     }
-  }, [token, key, setAuthToken]);
+  }, [authToken, key, timeOutFlag]);
 
-  return isValidToken;
+  return isValidRef.current;
 }
