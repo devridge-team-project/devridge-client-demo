2cda0ed84ffd8cb7395b71299fcee58146108a1b
duncan1409 <duncansitworld@gmail.com>
2024-03-30 14:25:52 +0900

    [Fix] not using hook

--- src/hook/qna/useQnaDetail.ts
+++
@@ -1,9 +0,0 @@
-import { qna } from "connection";
-import { useEffect } from "react";
-export default function useQnaDetail(id: number) {
-  useEffect(() => {
-    (async () => {
-      const response = await qna.get(id);
-    })();
-  }, []);
-}
