e154619404d3387db1865d029bb045bea10dafe5
jyj1111 <jyj202040@gmail.com>
2024-04-08 23:13:47 +0900

    [Style]: 안 쓰닌 링크 주석

--- src/asset/navigation.ts
+++ src/asset/navigation.ts
@@ -10,21 +10,21 @@ export const navigations = [
     href: "/questions",
     icon: "circle-and-square.svg",
   },
-  {
+  /* {
     name: "스터디 및 프로젝트",
     href: "/team",
     icon: "writing.svg",
-  },
+  } */
   {
     name: "커뮤니티",
     href: "/community",
     icon: "people.svg",
   },
-  {
+  /* {
     name: "공지사항",
     href: "/notice",
     icon: "notice.svg",
-  },
+  } */
   {
     isSignIn: true,
     name: "커피챗",
